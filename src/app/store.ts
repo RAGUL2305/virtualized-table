9cff823caf50aa8894c102639a23c29c3d03184e
RAGUL2305 <dhoniragul0@gmail.com>
2024-10-04 13:25:50 +0530

    add virtualAPI reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,16 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import virtualTableAPI from "../features/virtual-table/VirtualTableApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
-    
+    [virtualTableAPI.reducerPath]: virtualTableAPI.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(virtualTableAPI.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
